2571444e8cf0fcd0456b0294e91976710ee26580
kokibas <nurym0807@gmail.com>
2022-10-28 12:11:26 +0600

    Final Task

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -2,8 +2,8 @@ package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
     public void toFahrenheit(int temperatureCelsius){
-        double fahrenheit ;
-        temperatureCelsius =  (fahrenheit - 32) * 5/9 ;
-        System.out.println(temperatureCelsius);
+        double fahrenheit = 0;
+        fahrenheit  = ( temperatureCelsius * 1.8) + 32;
+        System.out.println(fahrenheit);
     }
 }
