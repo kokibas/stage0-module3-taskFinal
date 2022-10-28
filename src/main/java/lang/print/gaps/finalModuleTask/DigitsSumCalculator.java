744bc0f8d9e9b41001d9e74a595d1fe31c1b62a2
kokibas <nurym0807@gmail.com>
2022-10-28 10:30:36 +0600

    Final Task

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -1,11 +1,11 @@
 package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
-    public void calculateSum() {
-        int number = 3636;
+    public void calculateSum(int number) {
+
         int sum = 0;
-        sum +=  number % 10;
-        number = number / 10;
+        sum +=  (number % 10) +(number / 10) ;
+
         System.out.println(sum);
 
     }
